0fecb82ae784aece813fc660886ed63a18742c9a
Nozomu Kaneko <nozom.kaneko@gmail.com>
2018-09-13 18:27:19 +0900

    initial

---
+++ main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type ReceiverFunc func(key string, value float64)
+
+func (receiver ReceiverFunc) Receive(key string, value float64) {
+	receiver(key, value)
+}
+
+type Receiver interface {
+	Receive(key string, value float64)
+}
+
+func WalkJSON(path string, jsonData interface{}, receiver Receiver) {
+	switch v := jsonData.(type) {
+	case int:
+		receiver.Receive(path, float64(v))
+	case float64:
+		receiver.Receive(path, v)
+	case bool:
+		n := 0.0
+		if v {
+			n = 1.0
+		}
+		receiver.Receive(path, n)
+	case string:
+		// ignore
+	case nil:
+		// ignore
+	case []interface{}:
+		prefix := path + "__"
+		for i, x := range v {
+			WalkJSON(fmt.Sprintf("%s%d", prefix, i), x, receiver)
+		}
+	case map[string]interface{}:
+		var prefix string
+		if path != "" {
+			prefix = path + "."
+		} else {
+			prefix = ""
+		}
+		for k, x := range v {
+			WalkJSON(fmt.Sprintf("%s%s", prefix, k), x, receiver)
+		}
+	default:
+		log.Printf("unkown type: %#v", v)
+	}
+}
+
+func doProbe(target string) (interface{}, error) {
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+
+	resp, err := client.Get(target)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var jsonData interface{}
+	err = json.Unmarshal([]byte(bytes), &jsonData)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
+}
+
+func probeHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+
+	target := params.Get("target")
+	if target == "" {
+		http.Error(w, "Target parameter is missing", http.StatusBadRequest)
+		return
+	}
+
+	jsonData, err := doProbe(target)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	// log.Printf("Retrieved value %v", jsonData)
+
+	registry := prometheus.NewRegistry()
+
+	WalkJSON("", jsonData, ReceiverFunc(func(key string, value float64) {
+		g := prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Name: key,
+				Help: "Retrieved value",
+			},
+		)
+		registry.MustRegister(g)
+		g.Set(value)
+	}))
+
+	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
+}
+
+var indexHTML = []byte(`<html>
+<head><title>Json Exporter</title></head>
+<body>
+<h1>Json Exporter</h1>
+<p><a href="/probe">Run a probe</a></p>
+<p><a href="/metrics">Metrics</a></p>
+</body>
+</html>`)
+
+func main() {
+	addr := flag.String("listen-address", ":9116", "The address to listen on for HTTP requests.")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write(indexHTML)
+	})
+	flag.Parse()
+	http.HandleFunc("/probe", probeHandler)
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
---
+++ main_test.go
@@ -0,0 +1,129 @@
+package main_test
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/shiroyagicorp/prometheus-json-exporter"
+)
+
+type kvPair struct {
+	key   string
+	value float64
+}
+
+type receiver struct {
+	received []kvPair
+}
+
+func (r *receiver) Receive(key string, value float64) {
+	r.received = append(r.received, kvPair{key, value})
+}
+
+func TestWalkJSON(t *testing.T) {
+	testData := []struct {
+		name     string
+		bytes    []byte
+		expected []kvPair
+	}{
+		{
+			name:  "float value",
+			bytes: []byte(`{"x": 1.0}`),
+			expected: []kvPair{
+				kvPair{key: "x", value: 1.0},
+			},
+		},
+		{
+			name:  "int value",
+			bytes: []byte(`{"x": 1}`),
+			expected: []kvPair{
+				kvPair{key: "x", value: 1.0},
+			},
+		},
+		{
+			name:  "bool value",
+			bytes: []byte(`{"x": true}`),
+			expected: []kvPair{
+				kvPair{key: "x", value: 1.0},
+			},
+		},
+		{
+			name:     "string value",
+			bytes:    []byte(`{"x": "ok"}`),
+			expected: nil,
+		},
+		{
+			name:     "null value",
+			bytes:    []byte(`{"x": null}`),
+			expected: nil,
+		},
+		{
+			name:  "array value",
+			bytes: []byte(`{"x": [1, 2, 3]}`),
+			expected: []kvPair{
+				kvPair{key: "x__0", value: 1},
+				kvPair{key: "x__1", value: 2},
+				kvPair{key: "x__2", value: 3},
+			},
+		},
+		{
+			name:  "nested value",
+			bytes: []byte(`{"x": {"y": 1}}`),
+			expected: []kvPair{
+				kvPair{key: "x.y", value: 1.0},
+			},
+		},
+		{
+			name:  "nested^2 value",
+			bytes: []byte(`{"x": {"y": {"z": 1}}}`),
+			expected: []kvPair{
+				kvPair{key: "x.y.z", value: 1.0},
+			},
+		},
+		{
+			name:  "array in nested value",
+			bytes: []byte(`{"x": {"y": [1, 2, 3]}}`),
+			expected: []kvPair{
+				kvPair{key: "x.y__0", value: 1},
+				kvPair{key: "x.y__1", value: 2},
+				kvPair{key: "x.y__2", value: 3},
+			},
+		},
+		{
+			name:  "array in array value",
+			bytes: []byte(`{"x": [[1, 2], [3, 4]]}`),
+			expected: []kvPair{
+				kvPair{key: "x__0__0", value: 1},
+				kvPair{key: "x__0__1", value: 2},
+				kvPair{key: "x__1__0", value: 3},
+				kvPair{key: "x__1__1", value: 4},
+			},
+		},
+		{
+			name:  "array at root",
+			bytes: []byte(`[1, 2, 3]`),
+			expected: []kvPair{
+				kvPair{key: "__0", value: 1},
+				kvPair{key: "__1", value: 2},
+				kvPair{key: "__2", value: 3},
+			},
+		},
+	}
+
+	for _, tt := range testData {
+		t.Run(tt.name, func(t *testing.T) {
+			var jsonData interface{}
+			err := json.Unmarshal(tt.bytes, &jsonData)
+			if err != nil {
+				t.Errorf("Error: %v", err)
+			}
+
+			r := &receiver{}
+			main.WalkJSON("", jsonData, r)
+			if !reflect.DeepEqual(r.received, tt.expected) {
+				t.Errorf("Got: %#v, expected: %#v", r.received, tt.expected)
+			}
+		})
+	}
+}
